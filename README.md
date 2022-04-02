4eec4f6d264a54b6dadac84627daba591e992e8f
Thungghuan <1092887426@qq.com>
2022-04-02 15:40:48 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nonogram
+
+> 挖坑，做个[nonogram](https://en.wikipedia.org/wiki/Nonogram)(数织)
+
+是这么个[玩意](https://cn.puzzle-nonograms.com/)
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <img w-20 h-20 rd-10 alt="Thungghuan" src="./assets/Thungghuan.jpg" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
