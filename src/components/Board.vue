045dc2ebb01206ce58cf6093793590487e1cd253
Thungghuan <1092887426@qq.com>
2022-04-04 17:29:24 +0800

    feat: update block style

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,10 +2,12 @@
 import { board, rows, cols, showAnswer, markType } from '../logic'
 
 const getBlockClass = (state: number) => {
-  if (state === 1) {
+  if (state === 0) {
+    return 'bg-gray-400 opacity-30 hover:opacity-90'
+  } else if (state === 1) {
     return 'bg-black'
   } else if (state === -1) {
-    return 'i-carbon-close text-red'
+    return 'text-red i-carbon-close opacity-100'
   }
 }
 
@@ -22,13 +24,8 @@ const markBlock = (row: number, column: number) => {
   <div flex="~ col">
     <div v-for="(_, r) in rows" flex>
       <template v-for="(_, c) in cols">
-        <div m="0.5" w8 h8 b box-border cursor-pointer>
-          <div
-            w8
-            h8
-            @click="!showAnswer && markBlock(r, c)"
-            :class="getBlockClass(board[r][c])"
-          />
+        <div m="0.5" w8 h8 cursor-pointer :class="getBlockClass(board[r][c])">
+          <div w8 h8 @click="!showAnswer && markBlock(r, c)" />
         </div>
       </template>
     </div>
