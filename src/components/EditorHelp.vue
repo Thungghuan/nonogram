cf0c3664af583b613f1613a1986f38e0fce86932
Thungghuan <1092887426@qq.com>
2022-04-20 12:14:19 +0800

    docs: update editor help modal

--- src/components/EditorHelp.vue
+++ src/components/EditorHelp.vue
@@ -55,46 +55,17 @@ const locale = useStorage('help-locale', 'en')
           </div>
         </div>
 
-        <div my3 max-w-80vw w-60ch indent-md text-sm text-justify text-gray-800>
+        <div my3 max-w-80vw w-60ch indent-md text-lg text-center text-gray-800>
           <template v-if="locale === 'en'">
-            <p>
-              Nonograms, also known as Hanjie, Paint by Numbers, Picross,
-              Griddlers, and Pic-a-Pix, and by various other names, are picture
-              logic puzzles in which cells in a grid must be colored or left
-              blank according to numbers at the side of the grid to reveal a
-              hidden picture. In this puzzle type, the numbers are a form of
-              discrete tomography that measures how many unbroken lines of
-              filled-in squares there are in any given row or column.
-            </p>
-            <p>
-              For example, a clue of "4 8 3" would mean there are sets of four,
-              eight, and three filled squares, in that order, with at least one
-              blank square between successive sets.
-            </p>
-            <p>
-              -- from
-              <a href="https://en.wikipedia.org/wiki/Nonogram">wikipedia</a>
-            </p>
+            <p>Customize your own nonogram puzzle now!</p>
+            <p>Paint your lattice, share it,</p>
+            <p>and let your firends figure it out! :)</p>
           </template>
           <template v-else-if="locale === 'zh-CN'">
-            <div text-left text-lg indent-lg>
-              <p>数织是一种逻辑游戏，以猜谜的方式绘画黑白位图。</p>
-              <p>
-                在一个网格中，每一行和列都有一组数，玩家需根据它们来填满或留空格子，最后就可以由此得出一幅图画。
-              </p>
-              <p>
-                例如，“4 8
-                3”的意思就是指该行或列上有三段连续的方块，分别占了4、8和3格，而每条线最少要由一个空格分开。
-              </p>
-              <p>
-                传统上，玩家是以黑色填满格子，和以“×”号标记一定不需要填充的格子。
-              </p>
-              <p text-right>
-                -- 参考
-                <a href="https://zh.wikipedia.org/wiki/%E6%95%B8%E7%B9%94">
-                  维基百科
-                </a>
-              </p>
+            <div>
+              <p>定制属于你的数织谜题！</p>
+              <p>点击方块创建你的点阵</p>
+              <p>然后分享给你的朋友吧～</p>
             </div>
           </template>
         </div>
